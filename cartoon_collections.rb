ce2363baad8a02e066d06e3bbe8d3ea0fad8217f
Brock Byrd <brockbyrdd@gmail.com>
2020-09-11 19:07:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ def summon_captain_planet(calls) #code an argument here
 
 def long_planeteer_calls(call_length)# code an argument here
   # Your code here
-  if calls.length > 4
+  if calls_length.length > 4
     true
   else
     false
