d8914914b6f1198d38d8cf25376bb9c20f889a92
Brock Byrd <brockbyrdd@gmail.com>
2020-09-11 19:04:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
 
 def summon_captain_planet(calls) #code an argument here
   # Your code here
-  calls.collect {|call| puts "#{call.capitalize}!"}
+  calls.collect {|call|  "#{call.capitalize}!"}
   end
 
 def long_planeteer_calls# code an argument here
