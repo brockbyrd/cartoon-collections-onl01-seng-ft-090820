817d083bdea63c64733b55a85fe70f1cde5c6476
Brock Byrd <brockbyrdd@gmail.com>
2020-09-11 18:10:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def roll_call_dwarves(names)# code an argument here
     i += 1
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet() #code an argument here
   # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
