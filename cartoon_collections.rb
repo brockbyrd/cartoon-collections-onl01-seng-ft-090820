7de8d044d716673d4f775edce773c0a72e8e1da7
Brock Byrd <brockbyrdd@gmail.com>
2020-09-11 19:21:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,20 +16,10 @@ def summon_captain_planet(calls) #code an argument here
   calls.collect {|call|  "#{call.capitalize}!"}
   end
 
-def long_planeteer_calls(words)# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
-  i = 0
-  word_length = []
-  while i < words.length
-    word_length << yield(words[i])
-    i += 1
-  end
   
-  if word_length.include?(false)
-    false
-  else
-    true
-  end
+  
 end
 
 def find_the_cheese# code an argument here
